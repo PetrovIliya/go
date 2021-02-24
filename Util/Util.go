8bcb4cbaa97b764a4d079881d5e49a091ba4d906
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2021-02-24 18:44:14 +0300

    Добавил первую лабу

---
+++ Util/Util.go
@@ -0,0 +1,28 @@
+package Util
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func readFile(fileName string) string {
+	file, err := os.Open(fileName)
+	if err != nil{
+		fmt.Println(err)
+		return ""
+	}
+	data := make([]byte, 64)
+
+	result := ""
+	for{
+		n, err := file.Read(data)
+		if err == io.EOF{
+			break
+		}
+		result += string(data[:n])
+	}
+
+	_ = file.Close()
+	return result
+}
